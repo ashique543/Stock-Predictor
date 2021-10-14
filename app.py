6165cb12d7c66c670c5f1d967c9452b5a87b1df7
Ashique Mullick <ashiquemullick14@gmail.com>
2021-10-14 21:14:51 +0530

    U

---
+++ app.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from flask import Flask, render_template ,request
+import pickle
+from flask.wrappers import Request
+import numpy as np
+
+app = Flask(__name__)
+
+modle=pickle.load(open('modle.pkl','rb'))
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    data=[[request.form['high'],request.form['low'],request.form['open'],request.form['volume']]]
+    predictions=modle.predict(data)
+    predictions=predictions.reshape(-1)[0]
+    return render_template('index.html',pred='{}'.format(predictions))
+
+if __name__=="__main__":
+    app.run(debug=True)
